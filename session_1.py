a4ff50c5a92b0caba7f1cf1ccd5c273ace66bd8c
Mohammad Heydari <Hmohammad2520@gmail.com>
2026-08-06 14:17:46 +0330

    Session 1

---
+++ session_1.py
@@ -0,0 +1,54 @@
+# In bakhsh baraye print ast
+
+# print(2 + 2)  # Karbord print
+# print((2 + 1) * 4)  # Karbord print
+
+# Var name
+length = 2  # Meghdardehi --> Tarif ham shod
+
+#print(length)
+
+
+# Ghanoon haye Meghdardehi:
+# 1. bayad ba harf shoro beshe
+# 2. dar Ebarat az yeseri charecter ha nemishe estefade kard: space-+/|\!@#$%^&*()[]
+# 3. Case Sensitive
+# 4. Esm haye Keyword Nemishe gozasht
+
+
+# Pep8
+# 1. snake case --> nam ha --> class_student
+# 2. esmi --> namayan gar meghdar bashe
+# 3. Zabani yechiz bahse behtare --> English
+
+
+# Var Values
+# Adadi
+length = 300_000_000  # int
+pi = 3.14  # float
+is_circle = True  # bool
+
+# String --> Reshte
+# immutble
+# Indexable
+# iterable
+name = 'Mohammad Heydari'
+pet = "Mohammad's cat"
+fromula = 'x+2'
+letter = 'Dear Mohammad\nHello'
+letter2 = '''
+Dear Parham
+I love you.
+'''
+
+# Bytes --> bstring
+image = b''
+
+# format string
+first_name = 'Mohammad'
+last_name = 'Heydari'
+age = 26
+
+print(first_name + ' ' + last_name)
+print(f'{first_name} {last_name}')
+print(f'{first_name}: {age * 365} is days old')
